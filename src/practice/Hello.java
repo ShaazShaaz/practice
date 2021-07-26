f5e81196111b94c1561a81727e5572ae715038d4
Shazia <shaziasenol@gmail.com>
2021-07-25 22:21:52 -0400

    just a commit

--- src/practice/Hello.java
+++ src/practice/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("work started");
         System.out.println("2. created repository");
+        System.out.println("hi hi hi");
     }
 }
