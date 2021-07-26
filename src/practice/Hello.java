63ba3ea920c0ca295d774b6093b084a9e0b389af
Shazia <shaziasenol@gmail.com>
2021-07-25 22:18:55 -0400

    added more work

--- src/practice/Hello.java
+++ src/practice/Hello.java
@@ -3,5 +3,6 @@ package practice;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("work started");
+        System.out.println("2. created repository");
     }
 }
